7a220569b9123c87ef516d6c2cc125c570176a5c
Russell Rogers <rrogerscg@gmail.com>
2022-12-18 13:57:08 -0800

    .

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
       database: env("DATABASE_NAME", "database-cms"),
-      user: env("DATABASE_USERNAME", ""),
+      user: env("DATABASE_USERNAME", "postgres"),
       password: env("DATABASE_PASSWORD", ""),
     },
     useNullAsDefault: true,
