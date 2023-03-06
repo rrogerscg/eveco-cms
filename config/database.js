4f7a0dc722097330a7dcdc0e6e310aa892acc1d0
Russell Rogers <rrogerscg@gmail.com>
2023-03-06 01:35:59 -0800

    changes

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ module.exports = ({ env }) => ({
     connection: {
       host: env("DATABASE_HOST", "127.0.0.1"),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "database-cms"),
-      user: env("DATABASE_USERNAME", "postgres"),
+      database: env("DATABASE_NAME", "eveco-dev"),
+      user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
     },
     useNullAsDefault: true,
